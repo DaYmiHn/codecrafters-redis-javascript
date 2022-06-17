e290f35cc34b10c63eae277affc15a3d0c648c66
Denis Savin <sawi.denis@yandex.ru>
2022-06-17 17:11:30 +0300

    test

--- app/main.js
+++ app/main.js
@@ -6,7 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer(socket => {
   socket.pipe(socket);
-  socket.end('reply="PONG"');
+  socket.end('+PONG\r\n');
 });
 
 server.listen(6379, '127.0.0.1');
