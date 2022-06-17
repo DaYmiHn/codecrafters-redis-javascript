10f1a5462bbc25f23769dc718a3c8987ac443a9a
Denis Savin <sawi.denis@yandex.ru>
2022-06-17 17:19:01 +0300

    test

--- app/main.js
+++ app/main.js
@@ -20,7 +20,7 @@ const server = net.createServer(socket => {
     console.log('client disconnected');
   });
   socket.on('data', () => {
-    socket.end('+PONG\r\n');
+    socket.write('+PONG\r\n');
   });
   socket.write('+PONG\r\n');
   socket.pipe(socket);
